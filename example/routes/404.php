46768c4330aee4ff52748fb6a53ef3e9acc3d64b
ChiperSoft <chiper@chipersoft.com>
2012-03-22 14:46:45 -0700

    little tweak

--- example/routes/404.php
+++ example/routes/404.php
@@ -1,4 +1,4 @@
 <?php 
 header((isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0').' 404 Not Found');
-echo '404';print_r($arguments);
+echo "404\n";print_r($arguments);
 
